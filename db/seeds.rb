ec21634c1919b9868a1110a170be48eaa3eb84cd
Brice <bricel67@gmail.com>
2022-09-29 08:39:27 +0200

    build user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: 'Lai-thi', password: 'password')
+User.create(username: 'Louis', password: 'password')
+User.create(username: 'Enzo', password: 'password')
+User.create(username: 'Antoine', password: 'password')
+User.create(username: 'Robin', password: 'password')
